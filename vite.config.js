3be8b6acf0c7187e790e742cfa3d7f462301efb0
Mustapha377 <143838046+Mustapha377@users.noreply.github.com>
2024-12-20 22:11:25 +0100

    success

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: 'https:mustapha377.github.io/Html.portfolio/', // Adjust this for GitHub Pages
+  base: '/https:mustapha377.github.io/Html.portfolio/', // Adjust this for GitHub Pages
   server: {
     port: 3000, // Change the dev server port if needed
   },
